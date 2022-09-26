d3c45029a6218bb841e34f86facd3da23fcb1eca
FedorowPavel <pavel.fedorov@exposit.com>
2022-09-26 21:22:27 +0300

    init login page layout added

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
 import {Outlet} from "react-router-dom";
 import TopBar from "./TopBar";
+import {Container} from "@mui/material";
 
 
 const Layout = () => {
   return (
     <>
       <TopBar/>
-      <Outlet/>
+      <Container sx={{
+        height: '100vh',
+      }}>
+        <Outlet/>
+      </Container>
     </>
+
   );
 };
 export default Layout;
--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import {Box, Card, Container, Typography} from "@mui/material";
+import {Link} from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      Login page
-    </div>
+    <Container maxWidth="xl" sx={{
+      minHeight: '100%',
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    }}>
+      <Box sx={{
+        minWidth: 275
+      }}>
+        <Card
+          variant="outlined"
+          sx={{
+            padding: '20px'
+          }}
+        >
+          <Typography
+            variant="h4"
+            color="text.primary"
+            gutterBottom
+          >
+            Login
+          </Typography>
+          <Link to='/registration'>Create account</Link>
+        </Card>
+      </Box>
+    </Container>
   );
 };
 
