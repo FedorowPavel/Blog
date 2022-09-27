baf6766c9d2d1d6e16e541603b241a4022649606
FedorowPavel <pavel.fedorov@exposit.com>
2022-09-27 17:42:05 +0300

    feature/login-page
    gray background added for main wrapper

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -2,14 +2,19 @@ import * as React from 'react';
 import {Outlet} from "react-router-dom";
 import TopBar from "./TopBar";
 import {Container} from "@mui/material";
+import {useTheme} from "@mui/styles";
+import {BlogTheme} from "@mui/material/styles";
 
 
 const Layout = () => {
+  const theme = useTheme<BlogTheme>()
+
   return (
     <>
       <TopBar/>
       <Container sx={{
         height: '100vh',
+        backgroundColor: theme.palette.grey["300"]
       }}>
         <Outlet/>
       </Container>
